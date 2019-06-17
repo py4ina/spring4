088633a88d841475390958c4bd0beb4770382391
py4ina <py4ina@gmail.com>
2019-06-17 10:16:50 +0300

    64 st

---
+++ src/main/java/com/apress/prospring4/ch3/BookwormOracle.java
@@ -0,0 +1,14 @@
+package com.apress.prospring4.ch3;
+
+public class BookwormOracle implements Oracle {
+    private Encyclopedia encyclopedia;
+
+    public void setEncyclopedia(Encyclopedia encyclopedia) {
+        this.encyclopedia = encyclopedia;
+    }
+
+    @Override
+    public String defineMeaningOfLfe() {
+        return "Encyclopedias are a waste of money - use the Internet";
+    }
+}
