1ef1826dad0b7e41f3d1d3ca08ae7deaae5d5b0e
py4ina <py4ina@gmail.com>
2019-06-22 22:00:15 +0300

    66 st

---
+++ src/main/java/com/apress/prospring4/ch3/XmlConfigWithBeanFactory.java
@@ -0,0 +1,17 @@
+package com.apress.prospring4.ch3;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.core.io.ClassPathResource;
+
+public class XmlConfigWithBeanFactory {
+    public static void main(String[] args) {
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+        reader.loadBeanDefinitions(new ClassPathResource("META-INF/spring/xml-bean-factory-config.xml"));
+
+        Oracle oracle = (Oracle) factory.getBean("oracle");
+        System.out.println(oracle.defineMeaningOfLfe());
+    }
+}
