0667ae3693e58cac577c4d4fd6a233bc0b4dc072
py4ina <py4ina@gmail.com>
2019-06-28 20:10:59 +0300

    76 st

---
+++ src/main/java/com/apress/prospring4/ch3/DeclareSpringComponents.java
@@ -0,0 +1,18 @@
+package com.apress.prospring4.ch3;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class DeclareSpringComponents {
+    public static void main(String[] args) {
+        GenericXmlApplicationContext context = new GenericXmlApplicationContext();
+        context.load("META-INF/spring/app-context-xml.xml");
+        context.refresh();
+
+//        MessageProvider messageProvider = context.getBean("messageProvider", MessageProvider.class);
+//        System.out.println(messageProvider.getMessage());
+
+        MessageRenderer messageRenderer = context.getBean("messageRenderer", MessageRenderer.class);
+        messageRenderer.render();
+
+    }
+}
--- src/main/java/com/apress/prospring4/ch3/annotation/StandardOutMessageRenderer.java
+++ src/main/java/com/apress/prospring4/ch3/annotation/StandardOutMessageRenderer.java
@@ -2,6 +2,7 @@ package com.apress.prospring4.ch3.annotation;
 
 import com.apress.prospring4.ch3.MessageProvider;
 import com.apress.prospring4.ch3.MessageRenderer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("messageRenderer")
@@ -18,6 +19,7 @@ public class StandardOutMessageRenderer implements MessageRenderer {
     }
 
     @Override
+    @Autowired
     public void setMessageProvider(MessageProvider provider) {
         this.messageProvider = provider;
     }
