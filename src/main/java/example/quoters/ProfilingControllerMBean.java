077717aeb5d0a49f43b47a4db00d6cc634138744
Vitalik <py4ina@gmail.com>
2019-08-06 10:58:50 +0300

    https://www.youtube.com/watch?v=BmBr5diz8WA&t=395s 45:42

---
+++ src/main/java/example/quoters/ProfilingControllerMBean.java
@@ -0,0 +1,5 @@
+package example.quoters;
+
+public interface ProfilingControllerMBean {
+    void setEnabled(boolean enabled);
+}
--- src/main/java/example/quoters/ProfilingHandlerBeanPostProcessor.java
+++ src/main/java/example/quoters/ProfilingHandlerBeanPostProcessor.java
@@ -2,19 +2,60 @@ package example.quoters;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.util.StopWatch;
 
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ProfilingHandlerBeanPostProcessor implements BeanPostProcessor {
     private Map<String, Class> map = new HashMap<>();
+    private ProfilingController controller = new ProfilingController();
+
+    public ProfilingHandlerBeanPostProcessor() throws Exception {
+        MBeanServer platformMBeanServer = ManagementFactory.getPlatformMBeanServer();
+        platformMBeanServer.registerMBean(controller, new ObjectName("profiling", "name","controller"));
+    }
+
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        Class<?> beanClass = bean.getClass();
+        if (beanClass.isAnnotationPresent(Profiling.class)){
+            map.put(beanName, beanClass);
+        }
         return bean;
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        return null;
+        Class beanClass = map.get(beanName);
+        if (beanClass != null) {
+            return Proxy.newProxyInstance(beanClass.getClassLoader(), beanClass.getInterfaces(), new InvocationHandler() {
+
+                @Override
+                public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
+                    if (controller.isEnabled()){
+                        System.out.println("Профилирую...");
+                        StopWatch stopWatch = new StopWatch();
+                        stopWatch.start();
+
+                        Object retVal = method.invoke(bean, objects);
+
+                        stopWatch.stop();
+                        System.out.println(stopWatch.getTotalTimeMillis());
+                        System.out.println("Все");
+                        return retVal;
+                    } else {
+                        return method.invoke(bean, objects);
+                    }
+                }
+            });
+        }
+        return bean;
     }
 }
