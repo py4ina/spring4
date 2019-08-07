921037c85976dc8fdfdfb620fb27175bb1a02511
Vitalik <py4ina@gmail.com>
2019-08-07 12:04:28 +0300

    Евгений Борисов — Spring-потрошитель, часть 2
    https://www.youtube.com/watch?v=cou_qomYLNU
    1:13

--- src/main/java/example/quoters/TerminatorQuoter.java
+++ src/main/java/example/quoters/TerminatorQuoter.java
@@ -25,7 +25,9 @@ public class TerminatorQuoter implements Quoter {
     }
 
     @Override
+    @PostProxy
     public void sayQuote() {
+        System.out.println("Phase 3");
         for (int i = 0; i < repeat; i++) {
             System.out.println(message);
         }
